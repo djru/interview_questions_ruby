1e1e0f756ed8d3f73acf5ce6dfc7188021246ee7
Daniel Ruswick <dan.ruswick@gmail.com>
2015-09-20 14:57:04 -0500

    fixes for first and last occurance

--- first_and_last_occurance_in_sorted_array.rb
+++ first_and_last_occurance_in_sorted_array.rb
@@ -1,30 +1,30 @@
-def first_occurance_in_sorted_array arr, n
-    low = 0
-    high = arr.length - 1
-    res = -1
-    
-    while low < high do
-        mid = (low + high) / 2
-        if arr[mid] == n
-            high = mid - 1
-            res = mid
-        elsif arr[mid] > n
-            high = mid - 1
-        else
-            low = mid - 1
-        end
-    end
-    return res
-end
-
-def last_occurance_in_sorted_array arr, n
+def first_occurrence_in_sorted_array arr, n
     low = 0
     high = arr.length - 1
     res = -1
     
     while low <= high do
         mid = (low + high) / 2
-        puts mid
+        if arr[mid] == n
+            high = mid - 1
+            res = mid
+        elsif arr[mid] > n
+            high = mid - 1
+        else
+            low = mid + 1
+        end
+    end
+    return res
+end
+
+def last_occurrence_in_sorted_array arr, n
+    low = 0
+    high = arr.length - 1
+    res = -1
+    
+    while low <= high do
+        
+        mid = (low + high) / 2
         if arr[mid] == n
             low = mid + 1
             res = mid
@@ -35,4 +35,16 @@ def last_occurance_in_sorted_array arr, n
         end
     end
     return res
+end
+
+# These two can be used to find the number of occurrences in a sorted array
+
+def num_occurrences arr, n
+	first = first_occurrence_in_sorted_array arr, n
+	last = last_occurrence_in_sorted_array arr, n
+	if first == -1 or last == -1
+		return 0
+	else
+		return 1 + last - first
+	end
 end
